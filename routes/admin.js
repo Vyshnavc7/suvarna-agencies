999aabe79fe8de87fc9683287dfecba9a0ba6607
Vyshnav <Vyshnavc235@gmail.com>
2022-03-10 23:53:43 +0530

    user dashboard,category,product links and pages added

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,27 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/all-products',{admin:true});
+  res.render('admin/dashboard',{admin:true});
 });  
 router.get('/add-category', function(req, res, next) {
   res.render('admin/add-category',{admin:true});
 }); 
 router.get('/add-product', function(req, res, next) {
   res.render('admin/add-product',{admin:true});
+}); 
+router.get('/view-edit-user', function(req, res, next) {
+  res.render('admin/view-edit-user',{admin:true});
+}); 
+router.get('/dashboard', function(req, res, next) {
+  res.render('admin/dashboard',{admin:true});
+}); 
+router.get('/all-products', function(req, res, next) {
+  res.render('admin/all-products',{admin:true});
+}); 
+router.get('/add-category', function(req, res, next) {
+  res.render('admin/add-category',{admin:true});
+}); 
+router.get('/view-category', function(req, res, next) {
+  res.render('admin/view-category',{admin:true});
 }); 
 module.exports = router;
