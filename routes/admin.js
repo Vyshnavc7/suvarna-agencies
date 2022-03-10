890eacfde900e3687c805eb70fc657f7b2441266
Vyshnav <Vyshnavc235@gmail.com>
2022-03-10 14:00:43 +0530

    path for admin buttons added for add category

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('admin/all-products',{admin:true});
 });  
+router.get('/add-category', function(req, res, next) {
+  res.render('admin/add-category',{admin:true});
+}); 
 module.exports = router;
