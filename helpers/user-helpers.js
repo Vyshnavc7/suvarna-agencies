805ccb3e171e3d62a1b84b24f3b40a481472bf67
Vyshnav <Vyshnavc235@gmail.com>
2022-04-27 13:35:43 +0530

    contact and feedback display in dshboard done

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,13 +1,13 @@
-var db=require('../config/connection')
-var collection=require('../config/collections');
+var db = require('../config/connection')
+var collection = require('../config/collections');
 const async = require('hbs/lib/async');
 // used for encryption of password
-const bcrypt= require('bcrypt');
+const bcrypt = require('bcrypt');
 const { promise, reject } = require('bcrypt/promises');
 const { parseWithoutProcessing } = require('handlebars');
-var objectID=require('mongodb').ObjectId
+var objectID = require('mongodb').ObjectId
 // signing up user
-module.exports={
+module.exports = {
     // addUser:(user)=>{
     //     return new promise(async(resolve,reject)=>{
     //         user.password=await bcrypt.hash(user.password,10)
@@ -15,127 +15,131 @@ module.exports={
     //             console.log('Data inserted');
     //             console.log(data);
     //             resolve(data)
-                
+
     //         })
     //     })
-       
+
     //  }, 
-    addUser:(user,callback)=>{
-       
+    addUser: (user, callback) => {
+
         // to insert datas into database in mongo
         // user.pass= bcrypt.hash(user.pass,10)
-            db.get().collection('user').insertOne(user).then((data)=>{
-                console.log('Data inserted');
-                callback(data)
-            })
-     },
-     doLogin:(user)=>{
-         return new Promise(async (resolve,reject)=>{
-             let loginStatus=false
-             let response={}
-            let user=await db.get().collection(collection.USER_COLLECTION).findOne({mail:user.email})
-            if(user){
-                if(pass==user.pass){
+
+        db.get().collection('user').insertOne(user).then((data) => {
+            console.log('User inserted');
+            callback(data)
+            resolve(data.ops[0])
+        })
+
+
+    },
+    doLogin: (user) => {
+        return new Promise(async (resolve, reject) => {
+            let loginStatus = false
+            let response = {}
+            let user = await db.get().collection(collection.USER_COLLECTION).findOne({ mail: user.email })
+            if (user) {
+                if (pass == user.pass) {
                     console.log('login');
                 }
             }
-         })
-     },
-     listUser:()=>{
-        return new Promise(async(resolve,reject)=>{
-            let use=await db.get().collection(collection.USER_COLLECTION).find().toArray()
+        })
+    },
+    listUser: () => {
+        return new Promise(async (resolve, reject) => {
+            let use = await db.get().collection(collection.USER_COLLECTION).find().toArray()
             resolve(use);
             console.log(use);
         })
-        
+
     },
-    deleteUser:(userID)=>{
-        return new Promise((resolve,reject)=>{
-            db.get().collection(collection.USER_COLLECTION).remove({_id:objectID(userID)}).then((response)=>{
+    deleteUser: (userID) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.USER_COLLECTION).remove({ _id: objectID(userID) }).then((response) => {
                 resolve(response)
             })
         })
     },
 
 
-    listProducts:()=>{
-        return new Promise(async(resolve,reject)=>{
-            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+    listProducts: () => {
+        return new Promise(async (resolve, reject) => {
+            let products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
             resolve(products)
             console.log(products);
         })
-        
+
     },
 
 
-    addStaff:(staff,callback)=>{
-       
+    addStaff: (staff, callback) => {
+
         // to insert datas into database in mongo
         // user.pass= bcrypt.hash(user.pass,10)
-            db.get().collection('staff').insertOne(staff).then((data)=>{
-                console.log('Data inserted');
-                callback(data)
-            })
-     },
-    
-    viewStaff:()=>{
-        return new Promise(async(resolve,reject)=>{
-            let staff=await db.get().collection(collection.STAFF_COLLECTION).find().toArray()
+        db.get().collection('staff').insertOne(staff).then((data) => {
+            console.log('Data inserted');
+            callback(data)
+        })
+    },
+
+    viewStaff: () => {
+        return new Promise(async (resolve, reject) => {
+            let staff = await db.get().collection(collection.STAFF_COLLECTION).find().toArray()
             resolve(staff);
             console.log(staff);
         })
-        
+
     },
 
-    deleteStaff:(staffID)=>{
-        return new Promise((resolve,reject)=>{
-            db.get().collection(collection.STAFF_COLLECTION).remove({_id:objectID(staffID)}).then((response)=>{
+    deleteStaff: (staffID) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.STAFF_COLLECTION).remove({ _id: objectID(staffID) }).then((response) => {
                 resolve(response)
             })
         })
     },
 
-    getStaffDetails:(staffID)=>{
-        return new Promise((resolve,reject)=>{
-            db.get().collection(collection.STAFF_COLLECTION).findOne({_id:objectID(staffID)}).then((staff)=>{
+    getStaffDetails: (staffID) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.STAFF_COLLECTION).findOne({ _id: objectID(staffID) }).then((staff) => {
                 resolve(staff)
             })
         })
     },
 
-    updateStaff:(staffID,staffDetails)=>{
-        return new Promise((resolve,reject)=>{
+    updateStaff: (staffID, staffDetails) => {
+        return new Promise((resolve, reject) => {
             db.get().collection(collection.STAFF_COLLECTION)
-            .updateOne({_id:objectID(staffID)},{
-                $set:{
-                    name:staffDetails.name,
-                    phone:staffDetails.phone,
-                    age:staffDetails.age,
-                    place:staffDetails.place
+                .updateOne({ _id: objectID(staffID) }, {
+                    $set: {
+                        name: staffDetails.name,
+                        phone: staffDetails.phone,
+                        age: staffDetails.age,
+                        place: staffDetails.place
 
-                }
-            }).then((response)=>{
-                resolve()
-            })
+                    }
+                }).then((response) => {
+                    resolve()
+                })
         })
     },
 
-    
 
-    contactSub:(contact,callback)=>{
-        db.get().collection('contact').insertOne(contact).then((data)=>{
-            console.log('contact is ',contact);
+
+    contactSub: (contact, callback) => {
+        db.get().collection('contact').insertOne(contact).then((data) => {
+            console.log('contact is ', contact);
             console.log('Contact Inserted');
             callback(data)
         })
     },
 
-    viewFeedback:()=>{
-        return new Promise(async(resolve,reject)=>{
-            let contact1=await db.get().collection(collection.FEEDBACK_COLLECTION).find().toArray()
+    viewFeedback: () => {
+        return new Promise(async (resolve, reject) => {
+            let contact1 = await db.get().collection(collection.FEEDBACK_COLLECTION).find().toArray()
             resolve(contact1);
             console.log(contact1);
         })
-        
+
     },
 }
--- routes/user.js
+++ routes/user.js
@@ -50,16 +50,40 @@ router.post('/contact', function (req, res, next) {
 
   userHelpers.contactSub(req.body, (result) => {
     res.render('user/contact', { admin: false })
-    
+
   })
 });
 
 
 router.post('/signup', (req, res) => {
-  userHelpers.addUser(req.body, (result) => {
-    res.render("user/index", { admin: false })
+
+  var db = require('../config/connection')
+  var collection = require('../config/collections');
+  var username = req.body.name
+  return new Promise(async(resolve,reject)=>{
+    var username1 = await db.get().collection(collection.USER_COLLECTION).find({},{name:username,_id:0}).toArray
+    resolve(username1)
+    console.log(username1);
+    if (username != username1) {
+      userHelpers.addUser(req.body, (result) => {
+        console.log('User',username1);
+        res.render("user/index", { admin: false ,result})
+      })
+    }else{
+      console.log('Already exist');
+  
+    }
+    })
+
+    // userHelpers.addUser(req.body).then((response)=>{
+    //   console.log(response);
+    // })
   })
-})
+  
+
+
+  
+
 
 router.post('/userLogin', (req, res) => {
   userHelpers.doLogin(req.body)
